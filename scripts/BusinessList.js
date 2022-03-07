8b73a12ffb987fee5f3020e371dea442c162694e
Nayhee <nathantraczewski@gmail.com>
2022-03-07 16:16:28 -0600

    updated

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -2,14 +2,21 @@ import { getBusinesses } from "./BusinessData.js";
 import { BusinessHTML } from "./Business.js";
 
 const businessElement = document.querySelector(".business")
+const NYElement = document.querySelector(".businessList--newYork")   
+
 
 export const businessList = () => {
-    const businessArray = getBusinesses();
-    businessElement.innerHTML = "<h1>Business List</h1>";
+    const businessArray = getBusinesses()
+    const newYorkArray = businessArray.filter(business => {
+        return business.addressStateCode === "NY"
+    })
 
-    businessArray.forEach(
-        (object) => {
-            businessElement.innerHTML += BusinessHTML(object)
-        }
-    );
+    businessArray.forEach(element => {
+        businessElement.innerHTML += BusinessHTML(element);
+    })
+
+    newYorkArray.forEach(element => {
+        NYElement.innerHTML += BusinessHTML(element);
+    })
+
 }

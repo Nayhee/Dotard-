30e61c17c6948585553edbd9e1c2ebb0decc219e
Nayhee <nathantraczewski@gmail.com>
2022-03-30 09:12:21 -0500

    Fixed the keypressEvent buggit add --all

--- scripts/main.js
+++ scripts/main.js
@@ -6,35 +6,39 @@ purchAgentHTML();
 
 
 
-const businesses = getBusinesses();
 
 const companySearchResultArticle = document.querySelector(".foundCompanies")
 
-document.querySelector("#companySearch")
-document.addEventListener("keypress", keyPressEvent => {
+const businesses = getBusinesses();
+
+document.querySelector("#companySearch").addEventListener("keypress", keyPressEvent => {
     if (keyPressEvent.charCode === 13) {
-        console.log(keyPressEvent)
-        //get the input element and grab its value.
-        //loop through the businesses objects and if any of the startWith inputElement.value then we identify that as the found business. 
-
-        const foundBusiness = businesses.find(element => element.companyName.includes(keyPressEvent.target));
-
-        companySearchResultArticle.innerHTML = `
-            <h2>
-            ${foundBusiness.companyName}
-            </h2>
-            <section>
-            ${foundBusiness.addressFullStreet}
-
-            </section>
-            <section>
-            ${foundBusiness.addressCity},
-            ${foundBusiness.addressStateCode}
-            ${foundBusiness.addressZipCode}
-            </section>
-            <br>
-        `;
+    
+        let lowercaseInputElementsValue = document.querySelector("#companySearch").value.toLowerCase();
+    
+        for (let business of businesses) {
+           console.log(business)
+           console.log(lowercaseInputElementsValue)
+    
+            let lowercaseCompanyName = business.companyName.toLowerCase(); //fix here
+    
+            if(lowercaseCompanyName.startsWith(lowercaseInputElementsValue)) {
+                companySearchResultArticle.innerHTML = `
+                    <h2>
+                    ${business.companyName}
+                    </h2>
+                    <section>
+                    ${business.addressFullStreet}
+        
+                    </section>
+                    <section>
+                    ${business.addressCity},
+                    ${business.addressStateCode}
+                    ${business.addressZipCode}
+                    </section>
+                    <br>
+                `;
+            }
+        }
     }
-});
-
-// 
+})
